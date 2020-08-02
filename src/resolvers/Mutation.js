002adbbed0c22fd29441f3575a0418504881c723
goreev <goreev.ar@gmail.com>
2020-08-02 22:17:27 +0300

    start

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,18 @@
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
+};
+
+module.exports = mutations;

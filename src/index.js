89955133a2c17cee376be4dccfd9d05a8f409e2d
goreev <goreev.ar@gmail.com>
2020-08-16 18:27:16 +0300

    added user signup mutation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
+const cookieParser = require("cookie-parser");
 
 const server = createServer();
+
+server.express.use(cookieParser());
 
 server.start(
   {
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const mutations = {
   async createItem(parent, args, ctx, info) {
     //TODO: Check if they are logged in
@@ -29,6 +32,29 @@ const mutations = {
     //TODO
     //3. Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+
+    const password = await bcrypt.hash(args.password, 10);
+
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] },
+        },
+      },
+      info
+    );
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    return user;
   },
 };
 

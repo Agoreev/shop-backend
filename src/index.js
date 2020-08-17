740620b83741d7d329f501fbcb777d0dde0d3561
goreev <goreev.ar@gmail.com>
2020-08-17 21:06:51 +0300

    added middleware for verifying users

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
+const jwt = require("jsonwebtoken");
 const db = require("./db");
 const cookieParser = require("cookie-parser");
 
 const server = createServer();
 
 server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {

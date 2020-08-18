fe152ec7f3bd77795454c0dcf21129072089b270
goreev <goreev.ar@gmail.com>
2020-08-18 21:51:20 +0300

    added sign in and out mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -56,6 +56,36 @@ const mutations = {
     });
     return user;
   },
+  async signin(parent, { email, password }, ctx, info) {
+    //1. check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    //2. Check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid password!");
+    }
+    //3. Generate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    //4. Set the cookie with new token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+
+    //5. Return the user
+    return user;
+  },
+  async signout(parent, args, ctx, info) {
+    const userId = ctx.request.userId;
+    if (!userId) {
+      throw new Error("Not logged in!");
+    }
+    ctx.response.clearCookie("token");
+    return userId;
+  },
 };
 
 module.exports = mutations;
